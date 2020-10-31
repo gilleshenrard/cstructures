73a489b1e164fb1ca68788c7cf66b49c85d2f362
Gilles Henrard <gilles.henrard@hotmail.com>
2020-10-31 12:30:50 +0100

    add comment headers

--- src/dataset_test.c
+++ src/dataset_test.c
@@ -1,5 +1,46 @@
 #include "dataset_test.h"
 
+/****************************************************************************************/
+/*  I : dataset record to print                                                         */
+/*  P : Prints an dataset record                                                        */
+/*  O : /                                                                               */
+/****************************************************************************************/
+#ifdef __GNUC__
+# pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+int Print_dataset(void* elem, void* nullable)
+{
+    dataset_t* A=(dataset_t*)elem;
+
+    printf("%2d,%32s%4.8f\n", A->id, A->type, A->price);
+    return 0;
+}
+
+/************************************************************/
+/*  I : record to summarise as a string                     */
+/*      /                                                   */
+/*  P : returns a string representing the dataset           */
+/*  O : /                                                   */
+/************************************************************/
+char* toString_dataset(void* elem)
+{
+    dataset_t* A = (dataset_t*)elem;
+
+    return A->type;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// DYNAMIC ALLOCATION METHODS /////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/****************************************************************************************/
+/*  I : first dataset to compare                                                        */
+/*      second dataset to compare                                                       */
+/*  P : Compares two datasets by their ID and name                                      */
+/*  O :  1 if A > B                                                                     */
+/*       0 if A = B                                                                     */
+/*      -1 if A < B                                                                     */
+/****************************************************************************************/
 int compare_dataset(void* a, void* b)
 {
     dataset_t* A=(dataset_t*)a, *B=(dataset_t*)b;
@@ -12,6 +53,14 @@ int compare_dataset(void* a, void* b)
         return strcmp(A->type, B->type);
 }
 
+/****************************************************************************************/
+/*  I : first dataset to compare                                                        */
+/*      second dataset to compare                                                       */
+/*  P : Compares two datasets by their ID                                               */
+/*  O :  1 if A > B                                                                     */
+/*       0 if A = B                                                                     */
+/*      -1 if A < B                                                                     */
+/****************************************************************************************/
 int compare_dataset_int(void* a, void* b)
 {
     dataset_t* A = (dataset_t*)a;
@@ -23,22 +72,4 @@ int compare_dataset_int(void* a, void* b)
         return -1;
     else
         return 0;
-}
-
-#ifdef __GNUC__
-# pragma GCC diagnostic ignored "-Wunused-parameter"
-#endif
-int Print_dataset(void* elem, void* nullable)
-{
-    dataset_t* A=(dataset_t*)elem;
-
-    printf("%2d,%32s%4.8f\n", A->id, A->type, A->price);
-    return 0;
-}
-
-char* toString_dataset(void* elem)
-{
-    dataset_t* A = (dataset_t*)elem;
-
-    return A->type;
 }
