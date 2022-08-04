a529060f76991d096eae20449f05a31afdd5cb53
Gilles Henrard <gilles.henrard@hotmail.com>
2022-08-04 19:59:27 +0200

    implement stacks algos

--- include/cstacks.h
+++ include/cstacks.h
@@ -1,6 +1,12 @@
 #ifndef CSTACKS_H_INCLUDED
 #define CSTACKS_H_INCLUDED
+#include "cstructurescommon.h"
 
+//queues manipulations
+int pushStack(meta_t*,  const void*);
+void* popStack(meta_t* meta);
 
+//queues functors
+int foreachStack(meta_t* meta, void* parameter, int (*doAction)(void*, void*));
 
 #endif // CSTACKS_H_INCLUDED
--- src/cstacks.c
+++ src/cstacks.c
@@ -1,1 +1,132 @@
+/*
+** cstacks.c
+** Library implementing the stacks
+** ---------------------------------------------------
+** Made by Gilles Henrard
+** Last modified : 04/08/2022
+*/
 #include "cstacks.h"
+#include <stdlib.h>
+
+/************************************************************/
+/*  I : Metadata necessary to the algorithm                 */
+/*      Element to add in the queue                         */
+/*  P : Push a new element at the head of the stack         */
+/*  O : 0 -> Element pushed                                 */
+/*     -1 -> Error                                          */
+/************************************************************/
+int pushStack(meta_t *meta,  const void *toAdd){
+    dyndata_t *newElement = NULL;
+
+    //check if meta data available
+    if(!meta)
+        return -1;
+
+    //check if element added is available
+    if(!toAdd)
+    {
+        if(meta->doPError)
+            (*meta->doPError)("pushStack: new element cannot be NULL");
+
+        return -1;
+    }
+
+    //allocate memory for the new element
+    if((newElement = allocate_dyn(meta, toAdd)) == NULL)
+    {
+        if(meta->doPError)
+            (*meta->doPError)("pushStack: new element could not be allocated");
+        return -1;
+    }
+
+    //if structure already exists
+    if(meta->structure){
+        //chain the element to the head of the stack
+        newElement->right = meta->structure;
+        ((dyndata_t *)meta->structure)->left = newElement;
+        meta->structure = newElement;
+
+        //increment the amount of elements
+        meta->nbelements++;
+    }
+    else{
+        //otherwise set all pointers and nbelements counter
+        meta->nbelements = 1;
+        meta->last = newElement;
+        meta->structure = newElement;
+    }
+
+    return 0;
+}
+
+/************************************************************/
+/*  I : Metadata necessary to the algorithm                 */
+/*  P : Pop the first element from the stack                */
+/*  O : Head of the stack                                   */
+/*      NULL if error or queue empty                        */
+/************************************************************/
+void* popStack(meta_t* meta){
+    dyndata_t *tmp = NULL;
+
+    //check if meta data available
+    if(!meta)
+        return NULL;
+
+    //get the first element of the stack
+    tmp = meta->structure;
+
+    //if not empty
+    if(tmp)
+    {
+        //update the head of the stack + update elements counter
+        meta->structure = tmp->right;
+        meta->nbelements--;
+
+        //if stack now empty, update pointers
+        if(!meta->nbelements){
+            meta->structure = NULL;
+            meta->last = NULL;
+        }
+    }
+
+    return tmp;
+}
+
+/************************************************************/
+/*  I : Metadata necessary to the algorithm                 */
+/*      Parameter for the action to perform                 */
+/*      Action to perform                                   */
+/*  P : Performs an action on every element of the stack    */
+/*  O : 0 -> OK                                             */
+/*     -1 -> Error                                          */
+/************************************************************/
+int foreachStack(meta_t* meta, void* parameter, int (*doAction)(void*, void*)){
+    dyndata_t *next = NULL, *current=NULL;
+
+    if(!meta)
+        return -1;
+
+    if(!doAction)
+    {
+        if(meta->doPError)
+            (*meta->doPError)("foreachStack: action to perform not defined");
+
+        return -1;
+    }
+
+    next = meta->structure;
+
+    while(next){
+        current = next;
+        next = next->right;
+        if((*doAction)(current->data, parameter) < 0)
+        {
+            if(meta->doPError)
+                (*meta->doPError)("foreachStack: action specified returned with an error");
+
+            return -1;
+        }
+    }
+
+    return 0;
+}
